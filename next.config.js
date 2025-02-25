9cf688e129946b5ef07bd628b0a84548ab9e4826
erwingsolorzano <71676268+erwingsolorzano@users.noreply.github.com>
2025-02-25 11:17:58 -0500

    main: Update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: isProd ? "/webpage" : "",
+  assetPrefix: isProd ? "/webpage/" : "",
 };
 
 module.exports = nextConfig;
