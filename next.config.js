af9ab9c7433458ca450db0c250e194afe8a4e517
erwingsolorzano <71676268+erwingsolorzano@users.noreply.github.com>
2025-02-25 11:42:28 -0500

    fix src img

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+console.log('🚬 ===> isProd:', isProd);
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: isProd ? "/webpage" : "",
+  assetPrefix: isProd ? "/webpage/" : "",
 };
 
 module.exports = nextConfig;
