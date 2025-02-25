abb0477e2aed4cc837583368718548b55916c876
erwingsolorzano <71676268+erwingsolorzano@users.noreply.github.com>
2025-02-25 11:29:42 -0500

    fix routes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  basePath: isProd ? "/webpage" : "",
-  assetPrefix: isProd ? "/webpage/" : "",
 };
 
 module.exports = nextConfig;
